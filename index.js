58e33d2ddbd95716ce332c2500fd0034cd476782
Big Trees <traveye@protonmail.com>
2023-01-26 16:21:37 -0800

    finishing class files, adding html template

--- index.js
+++ index.js
@@ -31,6 +31,37 @@ inquirer.prompt ([
 
 ]);
 
+// generate team cards
+
+
 // then we use fs to generate the the html file - need template to add in info
+
+html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Team Builder</title>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
+    />
+  </head>
+  <body>
+    <section class="hero is-small is-link">
+      <div class="hero-body has-text-centered">
+        <p class="title">My Team</p>
+      </div>
+    </section>
+    <section class="section">
+      <div class="container d-flex">
+        ${'cards'}
+      </div>
+    </section>
+  </body>
+</html>
+`
+
 
 // once the html page is open, i should be able to click on each email or ghub username to open either the email in defaul email app or ghub in new tab.
